7138932d7651a95a5b1d6f6b05b51f154e628165
Elinor Weissberg <elinorweissberg@gmail.com>
2023-10-04 10:19:58 -0400

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: './client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'development',
+        template: path.join(__dirname, 'index.html'),
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          // import css
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    devServer: {
+      port: 8080,
+      proxy: [
+        {
+          context: ['/api'],
+          target: 'http://localhost:3000',
+        },
+      ],
+      historyApiFallback: true,
+    },
+  };
