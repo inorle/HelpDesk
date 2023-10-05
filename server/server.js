4f590aa6e95f68f2da0f3cbbb39ffd3d93302101
Elinor Weissberg <elinorweissberg@gmail.com>
2023-10-05 00:41:42 -0400

    prog

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const newTicketController = require('./controllers/newTicketController')
 const adminTicketController = require('./controllers/adminTicketController')
 
