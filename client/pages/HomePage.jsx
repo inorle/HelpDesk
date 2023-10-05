9fb7cd0ff417e0ae945758ce9182116c176f69e7
Elinor Weissberg <elinorweissberg@gmail.com>
2023-10-05 01:28:07 -0400

    prog

--- client/pages/HomePage.jsx
+++ client/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TicketForm from '../components/TicketForm.jsx'
 const HomePage = () => {
     return (
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center justify-center'>
             <h2 className='mt-20 text-3xl font-bold '>Submit Help Desk Ticket</h2>
              <TicketForm /> 
             <a href='/admin' className='mt-3 link link-hover text-sm'> I'm an Admin </a>
